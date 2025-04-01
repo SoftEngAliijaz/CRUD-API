f4e49b796352f2045331241caa0b848b6d209d13
Muhammad Ali <softeng.aliijaz@gmail.com>
2025-04-02 02:25:38 +0500

    refactor server configuration for improved clarity and remove redundant error handling

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require("dotenv").config(); // Load environment variables (uf you have)
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const { configureDatabase } = require("./config");
@@ -8,31 +8,20 @@ const productRouter = require("./routes/product_route");
 const employeeRouter = require("./routes/employee_route");
 
 const app = express();
-const PORT = process.env.PORT || 9000;
-const DATABASE_URL =
-  process.env.DATABASE_URL || "mongodb://localhost:27017/crud-app";
+const PORT = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL;
 
-// Connect to Database
 configureDatabase(DATABASE_URL);
 
-// Middleware
-app.set("json spaces", 2);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan("dev")); // Logs requests
+app.use(morgan("dev"));
 
-// Routes
 app.use("/api/books", bookRouter);
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
 app.use("/api/employees", employeeRouter);
 
-// Global Error Handling (for unexpected errors)
-process.on("uncaughtException", (err) => {
-  console.error("🔥 Uncaught Exception:", err);
-  process.exit(1); // Exit the process
-});
-
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
